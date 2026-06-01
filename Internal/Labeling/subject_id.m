957fe5348487f451244567ee8d677423994732b9
linseyqiu-hub <linseyqiu@gmail.com>
2026-06-01 16:19:57 -0700

    fix typo in internal func

--- Internal/Labeling/subject_id.m
+++ Internal/Labeling/subject_id.m
@@ -1,9 +1,8 @@
 function sid = subject_id( sno )
-global scan_information
+    global scan_information
 
   sid = '';
   if size(scan_information.SubjectID, 2 ) >= sno 
     sid = char(scan_information.SubjectID( sno ));
-  end;
+  end
 
-1
